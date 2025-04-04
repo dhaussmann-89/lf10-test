5e75b96441e60417b7b8b1ba47c88c9129b633d5
David <david.haussmann@tls-giessen.eu>
2025-04-04 09:07:49 +0200

    feat: add timeinfo

--- hallo.py
+++ hallo.py
@@ -1,7 +1,9 @@
+import datetime
 def begruessung(name:str):
-print("Hallo", name, "!")
+     print("Hallo", name, "!")
 def tschuess(name:str):
  print("Auf Wiedersehen,", name, "!")
 name = input("Bitte geben Sie den Vornamen ein: ")
 begruessung(name)
+saytime()
 tschuess(name)
